9a7d6b7d169ee7308cb102adccee0da867d4fd22
mao yu <maoyu417@163.com>
2012-12-04 18:03:00 +0800

    库新项目文件

---
+++ AsiHttpRequestLibrary/AsiHttpRequestLibrary/AsiHttpRequestLibrary.h
@@ -0,0 +1,13 @@
+//
+//  AsiHttpRequestLibrary.h
+//  AsiHttpRequestLibrary
+//
+//  Created by lixiaoyu on 12-12-4.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AsiHttpRequestLibrary : NSObject
+
+@end
